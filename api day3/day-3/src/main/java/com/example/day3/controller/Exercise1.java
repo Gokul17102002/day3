5ac77b90c1e83d134d2268a4ab9d830970b362af
Gokul17102002 <128041109+Gokul17102002@users.noreply.github.com>
2023-08-22 16:33:16 +0530

    Add files via upload

---
+++ api day3/day-3/src/main/java/com/example/day3/controller/Exercise1.java
@@ -0,0 +1,21 @@
+package com.example.day3.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Exercise1 {
+@Value("1")
+public int id;
+@Value("Gokul")
+public String name;
+@Value("19")
+public int age;
+@Value("20000")
+public long salary;
+@RequestMapping("/day")
+public String day() {
+	return id+"-"+name+"-"+age+"-"+salary;
+}
+}
+
